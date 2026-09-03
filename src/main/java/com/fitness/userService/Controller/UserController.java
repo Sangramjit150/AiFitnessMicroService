a965c0de9bb299bb891b830b269b9c43a2d76711
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-09-03 19:27:04 +0530

    We are setting up the User Services and the controllers and the dtos for our application

--- src/main/java/com/fitness/userService/Controller/UserController.java
+++ src/main/java/com/fitness/userService/Controller/UserController.java
@@ -1,4 +1,28 @@
 package com.fitness.userService.Controller;
 
+import com.fitness.userService.dto.RegisterRequest;
+import com.fitness.userService.dto.UserResponse;
+import com.fitness.userService.models.User;
+import com.fitness.userService.service.UserService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
 public class UserController {
+
+    private UserService userService;
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse>getUserById(@PathVariable Long id){
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse>registerUser(@Valid @RequestBody RegisterRequest registerRequest){
+        return ResponseEntity.ok(registerRequest.register(registerRequest));
+    }
 }
--- src/main/java/com/fitness/userService/service/UserService.java
+++ src/main/java/com/fitness/userService/service/UserService.java
@@ -1,4 +1,8 @@
 package com.fitness.userService.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
 }
