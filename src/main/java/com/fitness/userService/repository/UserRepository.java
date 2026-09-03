a8011d8c5be38c45ff4df8f9c4bbaa7fae9398a5
Sangramjit150 <sangramjitbaksi@gmail.com>
2026-09-03 20:52:15 +0530

    The user repository layer for interacting with the database

--- src/main/java/com/fitness/userService/repository/UserRepository.java
+++ src/main/java/com/fitness/userService/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.fitness.userService.repository;
 
-public interface UserRepository {
+import com.fitness.userService.models.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    boolean existsByEmail(@NotBlank(message = "Email is required") @Email(message = "Invalid email format") String email);
 }
